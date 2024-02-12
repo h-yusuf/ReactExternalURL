09ed5f5951be979b4e6edaf049c364f935d69e58
h-yusuf <136818715+h-yusuf@users.noreply.github.com>
2024-02-12 15:56:39 +0000

    fix : external url

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
+// import { UserAgent } from 'react-useragent';
 
 function App() {
+  useEffect(() => {
+    if (navigator.userAgent.includes("Instagram")) {
+      window.open("https://react-external-urls.vercel.app/", "_blank")
+    }
+  }, [])
   return (
     <div className="App">
+      {/* <UserAgent>
+        {({ ua }) => {
+          return ua.mobile ? <input type="date" /> : <input type="text" /> >;
+        }}
+      </UserAgent> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
