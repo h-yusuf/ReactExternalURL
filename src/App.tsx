5457c057f56f237450d033131b9b732a56bc43c6
h-yusuf <136818715+h-yusuf@users.noreply.github.com>
2024-02-12 16:52:46 +0000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
-// import { UserAgent } from 'react-useragent';
 
 function App() {
   useEffect(() => {
     if (navigator.userAgent.includes("Instagram")) {
-      window.open("https://react-external-urls.vercel.app/", "_blank")
+      const confirmMessage = "Anda membuka aplikasi dari Instagram. Klik OK untuk membuka link di browser eksternal.";
+      if (window.confirm(confirmMessage)) {
+        window.open("https://react-external-urls.vercel.app/", "_blank");
+      }
     }
-  }, [])
+  }, []);
+
   return (
     <div className="App">
-      {/* <UserAgent>
-        {({ ua }) => {
-          return ua.mobile ? <input type="date" /> : <input type="text" /> >;
-        }}
-      </UserAgent> */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
